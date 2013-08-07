dbf3e42a76adf398ac938836c04ada0bde137b1d
Alexey Ershov <alex4rom@gmail.com>
2013-08-07 14:11:00 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coalatravel::Application.config.secret_token = '91aa7af60ec24392fa6142d576e611995b59fd6c230b08c417fa02c392ebec437a23c0df4b6cfb95efb2ee14b06c1279f8ce766d18348aba0ef512ef738b0561'
