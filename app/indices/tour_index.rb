6a852fd1db8e1d2feaaa7c5903766b6253663a18
Alexey Ershov <alex4rom@gmail.com>
2013-08-14 15:06:50 +0300

    Add sphinx filters

--- app/indices/tour_index.rb
+++ app/indices/tour_index.rb
@@ -1,7 +1,11 @@
 ThinkingSphinx::Index.define :tour, :with => :active_record do
   #fields
   
-  indexes direction, country, cost, summary
-  indexes :name
+  indexes summary
+  indexes country, direction, cost, sortable: true
+  #indexes :name, sortable: true
+
+  # attributes
+  #has cost
 
 end
