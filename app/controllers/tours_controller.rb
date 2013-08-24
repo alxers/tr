e1eea6d76b55a72801d9f3600b416687ec5ffc1f
Alexey Ershov <alex4rom@gmail.com>
2013-08-24 14:26:37 +0300

    Add filter fields for search engines

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,9 +1,11 @@
 class ToursController < ApplicationController
   def index
     if params[:engine] == "elastic"
-      @tours = Tour.tire.search(params[:query], load: true)
+      @tours = Tour.tire.search "#{params[:query]} #{params[:name]} #{params[:cost]}
+                                 #{params[:direction]} #{params[:country]}", load: true
     elsif params[:engine] == "sphinx"
-      @tours = Tour.search(params[:query])
+      @tours = Tour.search "#{params[:query]} #{params[:name]} #{params[:cost]}
+                            #{params[:direction]} #{params[:country]}", :match_mode => :any
     else
       @tours = Tour.all
     end
