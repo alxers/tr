d3723ea68f89bcec4658b8287cc31b9b2dad5094
Alexey Ershov <alex4rom@gmail.com>
2013-08-27 13:36:11 +0300

    Add cost filters

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -4,8 +4,8 @@ class ToursController < ApplicationController
       @tours = Tour.tire.search "#{params[:query]} #{params[:name]} #{params[:cost]}
                                  #{params[:direction]} #{params[:country]}", load: true
     elsif params[:engine] == "sphinx"
-      @tours = Tour.search "#{params[:query]} #{params[:name]} #{params[:cost]}
-                            #{params[:direction]} #{params[:country]}", :match_mode => :any
+      @tours = Tour.search "#{params[:query]} #{params[:name]} #{params[:direction]} #{params[:country]}", 
+                            :with => {:cost => params[:cost_from]..params[:cost_to]}, :match_mode => :any
     else
       @tours = Tour.all
     end
--- app/indices/tour_index.rb
+++ app/indices/tour_index.rb
@@ -2,10 +2,10 @@ ThinkingSphinx::Index.define :tour, :with => :active_record do
   #fields
   
   indexes summary
-  indexes country, direction, cost, sortable: true
+  indexes country, direction, sortable: true
   indexes :name, sortable: true
 
   # attributes
-  #has cost
+  has cost
 
 end
