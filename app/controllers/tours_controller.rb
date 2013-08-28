3db6cc7bd14e91768d1c568bc3090dd72c4dd88a
Alexey Ershov <alex4rom@gmail.com>
2013-08-28 12:51:01 +0300

    Fix elasticsearch filters

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,11 +1,13 @@
 class ToursController < ApplicationController
   def index
     if params[:engine] == "elastic"
-      @tours = Tour.tire.search "#{params[:query]} #{params[:name]} #{params[:cost]}
-                                 #{params[:direction]} #{params[:country]}", load: true
+      @tours = Tour.elastic_search(params)
     elsif params[:engine] == "sphinx"
-      @tours = Tour.search "#{params[:query]} #{params[:name]} #{params[:direction]} #{params[:country]}", 
-                            :with => {:cost => params[:cost_from]..params[:cost_to]}, :match_mode => :any
+      @tours = Tour.search("#{params[:query]} #{params[:name]} 
+                            #{params[:direction]} #{params[:country]}", 
+                            :with => {:cost => params[:cost_from]..params[:cost_to]}, 
+                            #:match_mode => :any
+                            )
     else
       @tours = Tour.all
     end
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -3,4 +3,17 @@ class Tour < ActiveRecord::Base
 
   include Tire::Model::Search
   include Tire::Model::Callbacks
+
+  def self.elastic_search(params)
+    tire.search do
+      query { string params[:query] } if params[:query].present?
+      query { string params[:name] } if params[:name].present?
+      query { string params[:direction] } if params[:direction].present?
+      query { string params[:country] } if params[:country].present?
+      #filter :range, cost: { from: params[:cost_from], to: params[:cost_to] }
+      filter :range, cost: { from: params[:cost_from] } if params[:cost_from].present?
+      filter :range, cost: { to: params[:cost_to] } if params[:cost_to].present?
+    end
+  end
+
 end
