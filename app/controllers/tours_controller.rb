efccb05e00edc6a45ed139f61cfb69997e6c8282
Alexey Ershov <alex4rom@gmail.com>
2013-08-29 11:07:03 +0300

    Fix sphinx filters

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,13 +1,25 @@
 class ToursController < ApplicationController
   def index
+    query = [:query, :name, :direction, :country].select { |key|
+      params[key].present?
+    }.collect { |key| params[key] }.join(' ') 
+    filters = {}
+    #if params[:cost_from].present? && params[:cost_to].present?
+     # filters[:cost] = params[:cost_from]..params[:cost_to]
+    #end
+
+    if params[:cost_from].present?
+      filters[:cost] = params[:cost_from]..99999.to_s
+    elsif params[:cost_to].present?
+      filters[:cost] = 0.to_s..params[:cost_to]
+    elsif params[:cost_from].present? && params[:cost_to].present?
+      filters[:cost] = params[:cost_from]..params[:cost_to]
+    end
+    
     if params[:engine] == "elastic"
       @tours = Tour.elastic_search(params)
     elsif params[:engine] == "sphinx"
-      @tours = Tour.search("#{params[:query]} #{params[:name]} 
-                            #{params[:direction]} #{params[:country]}", 
-                            :with => {:cost => params[:cost_from]..params[:cost_to]}, 
-                            #:match_mode => :any
-                            )
+      @tours = Tour.search query, :with => filters
     else
       @tours = Tour.all
     end
